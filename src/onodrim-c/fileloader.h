931d69f771466b80c2bed36b4b8e0c6307ef6015
Emil Ekberg <ekberg.emil@gmail.com>
2017-12-18 22:44:27 +0100

    updated project files

--- src/onodrim-c/fileloader.h
+++ src/onodrim-c/fileloader.h
@@ -16,10 +16,10 @@ public:
 	static void OnLoad(void* argv, void* bufferPtr, int bufferLength)
 	{
 		auto arr = static_cast<std::string*>(argv);
-		
+		onodrim::utils::log("Finished loading %s", (arr));
 		const char* buffer = static_cast<const char*>(bufferPtr);
 		std::string content(buffer, bufferLength);
-		onodrim::utils::log("OnLoad %s", (arr));
+		
 		delete arr;
 		onodrim::utils::log("OnLoad %s %i", content.c_str(), bufferLength);
 	}
@@ -30,15 +30,16 @@ public:
 		onodrim::utils::log("OnError %s", argv);
 	}
 
-	static void ReadFile()
+	static void ReadFile(const std::string& path)
 	{
-		std::string* str = new std::string("test");
-		const char* path = "bin/sprite.frag";
-		std::string argv[1] = { "Something" };
-		onodrim::utils::log("%i", sizeof(argv));
-		emscripten_async_wget_data(path, str, &OnLoad, &OnError);
+		std::string* identifier = new std::string("test");
+		// const char* path = "bin/sprite.frag";
+		onodrim::utils::log("ReadFile %s %s", path.c_str(), identifier->c_str());
+		emscripten_async_wget_data(path.c_str(), identifier, &OnLoad, &OnError);
 	}
 #endif
 protected:
+
+
 };
 
