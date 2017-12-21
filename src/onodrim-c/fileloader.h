23136253242d9de4b1db5f302bee02bae27556bb
Emil Ekberg <ekberg.emil@gmail.com>
2017-12-21 19:24:20 +0100

    fileloader now works somewhat ok

--- src/onodrim-c/fileloader.h
+++ src/onodrim-c/fileloader.h
@@ -6,40 +6,71 @@
 #include <string>
 #include "./utils/logger.h"
 #include <functional>
-class FileLoader
+#include <future>
+#include <chrono>
+#include <memory>
+using namespace onodrim::utils;
+namespace FileLoader
 {
-public:
-	FileLoader();
-	~FileLoader();
-
+	
 #ifdef __EMSCRIPTEN__
-	static void OnLoad(void* argv, void* bufferPtr, int bufferLength)
+	typedef std::function<void(std::string path, bool success, std::string data)> FileLoaderCallback;
+	struct FileResult
 	{
-		auto arr = static_cast<std::string*>(argv);
-		onodrim::utils::log("Finished loading %s", (arr));
+		std::string path;
+		FileLoaderCallback callback;
+	};
+	void OnLoad(void* void_ptr_result, void* bufferPtr, int bufferLength)
+	{
 		const char* buffer = static_cast<const char*>(bufferPtr);
 		std::string content(buffer, bufferLength);
-		
-		delete arr;
-		onodrim::utils::log("OnLoad %s %i", content.c_str(), bufferLength);
+		FileResult* result = static_cast<FileResult*>(void_ptr_result);
+		result->callback(result->path, true, content);
+		delete result;
 	}
 
-	static void OnError(void* argv)
+	void OnError(void* void_ptr_result)
 	{
-		const char* arr = static_cast<const char*>(argv);
-		onodrim::utils::log("OnError %s", argv);
+		FileResult* result = static_cast<FileResult*>(void_ptr_result);
+		result->callback(result->path, false, "");
+		delete result;
 	}
 
-	static void ReadFile(const std::string& path)
+	void ReadFile(const std::string& path, FileLoaderCallback callback)
 	{
-		std::string* identifier = new std::string("test");
-		// const char* path = "bin/sprite.frag";
-		onodrim::utils::log("ReadFile %s %s", path.c_str(), identifier->c_str());
-		emscripten_async_wget_data(path.c_str(), identifier, &OnLoad, &OnError);
+		FileResult* result = new FileResult();
+		result->callback = callback;
+		result->path = path;
+		emscripten_async_wget_data(path.c_str(), result, &OnLoad, &OnError);
 	}
 #endif
-protected:
+}
+/*
+	void OnLoadPromise(void* argv, void* bufferPtr, int bufferLength)
+	{
+		const char* buffer = static_cast<const char*>(bufferPtr);
+		std::string content(buffer, bufferLength);
+		std::promise<std::string>* promise = static_cast<std::promise<std::string>*>(argv);
+		promise->set_value(content);
+	}
+	std::string ReadFilePromise(const std::string& path)
+	{
+		std::promise<std::string>* readfile_promise = new std::promise<std::string>();
+		std::future<std::string> future = readfile_promise->get_future();
+		emscripten_async_wget_data(path.c_str(), (void*)readfile_promise, OnLoadPromise, OnError);
+		std::string result = future.get();
+		delete readfile_promise;
+		return result;
+	}
 
-
-};
-
+	std::string ReadFileSync(const std::string& path)
+	{
+		emscripten_wget(path.c_str(), path.c_str());
+		FILE* img;
+		img=fopen(path.c_str(), "r");
+		onodrim::utils::log(path.c_str());
+		return "";
+		// onodrim::utils::log(img);
+	}
+}
+*/
